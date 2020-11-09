363d9e9b1f1197877c6bcf5d405bc35201ec2f68
mrFarno <felicienarnault@gmail.com>
2020-11-09 14:57:54 +0100

    fix eqdao+spdao

--- app/DAO/EquipmentDAO.php
+++ app/DAO/EquipmentDAO.php
@@ -12,15 +12,17 @@ class EquipmentDAO extends DAO
 
     public function persist($datas)
     {
-        $cc_dao = new CleaningContextDAO([
-            'db_host' => $this->host,
-            'db_user' => $this->user,
-            'db_pass' => $this->password,
-            'db_name' => $this->db_name,
-            'db_type' => $this->type
-        ]);
-        $id = $cc_dao->persist(['cc_id' => null]);
-        $datas['eq_id'] = $id;
+        if(!isset($datas['eq_id'])) {
+            $cc_dao = new CleaningContextDAO([
+                'db_host' => $this->host,
+                'db_user' => $this->user,
+                'db_pass' => $this->password,
+                'db_name' => $this->db_name,
+                'db_type' => $this->type
+            ]);
+            $id = $cc_dao->persist(['cc_id' => null]);
+            $datas['eq_id'] = $id;
+        }
         return parent::persist($datas);
     }
 }
